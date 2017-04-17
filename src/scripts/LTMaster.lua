58f2d48e4009e659476fb56954fb94b4d7cdc9bc
Matteo Autieri <matteoautieri@live.it>
2017-04-18 01:23:47 +0200

    first step

--- src/scripts/LTMaster.lua
+++ src/scripts/LTMaster.lua
@@ -1,88 +1,83 @@
 --
--- LTMaster
+-- Goweil LT Master
 --
 -- Team FSI Modding
--- 
--- 2017-03
 --
-
--- Appunti poi da cancellare... spec di default Baler
---			<specialization name="attacherJoints" />
---            <specialization name="lights" />
---            <specialization name="workArea" />
---            <specialization name="speedRotatingParts" />
---            <specialization name="attachable" />
---            <specialization name="turnOnVehicle" />
---            <specialization name="animatedVehicle" />
---            <specialization name="cylindered" />
---            <specialization name="foldable" />
---            <specialization name="fillable" />
---            <specialization name="fillVolume" />
---            <specialization name="pickup" />
---            <specialization name="baler" />
---            <specialization name="powerConsumer" />
---            <specialization name="washable" />
---            <specialization name="mountable" />
---
-
-
+-- 18/04/2017
 LTMaster = {};
+LTMaster.debug = true
+
+function LTMaster.print(text, ...)
+    if LTMaster.debug then
+        local start = string.format("[%s(%s)] -> ", "LTMaster", getDate("%H:%M:%S"));
+        local ptext = string.format(text, ...);
+        print(string.format("%s%s", start, ptext));
+    end
+end
 
 function LTMaster.prerequisitesPresent(specializations)
-    return SpecializationUtil.hasSpecialization(Baler, specializations) and SpecializationUtil.hasSpecialization(AnimatedVehicle, specializations);
+    return true;
 end
 
-
-function LTMaster:preLoad(savegame)	
-
-end
-
-
-function LTMaster:load(savegame)	
+function LTMaster:preLoad(savegame)
 
 end
 
+function LTMaster:load(savegame)
+    self.LTMaster = {};
+    local triggerId = Utils.indexToObject(self.components, getXMLString(self.xmlFile, "vehicle.LTMaster.triggers.triggerLeft#index"));
+    addTrigger(triggerId, "playerTriggerLeft", self);
+    triggerId = Utils.indexToObject(self.components, getXMLString(self.xmlFile, "vehicle.LTMaster.triggers.triggerRight#index"));
+    addTrigger(triggerId, "playerTriggerRight", self);
+end
 
 function LTMaster:postLoad(savegame)
 
 end
 
-
 function LTMaster:delete()
 
 end
-
 
 function LTMaster:mouseEvent(posX, posY, isDown, isUp, button)
 
 end
 
-
 function LTMaster:keyEvent(unicode, sym, modifier, isDown)
 
 end
 
-
 function LTMaster:update(dt)
-	if self:getIsActive() then
-		-- ma... esattamente che significa getIsActive...
-	end;
 end
-
 
 function LTMaster:updateTick(dt)
 
 end
 
-
 function LTMaster:draw()
 
 end
 
-
-function LTMaster:setupLTMaster()
-
+function LTMaster:playerTriggerLeft(triggerId, otherId, onEnter, onLeave)
+    if g_currentMission.player ~= nil and g_currentMission.player.rootNode == otherId then
+        if onEnter then
+            self.triggerLeftActive = true;
+        end
+        if onLeave then
+            self.triggerLeftActive = false;
+        end
+        LTMaster.debug("self.triggerLeftActive:%s", self.triggerLeftActive);
+    end
 end
 
-
-
+function LTMaster:playerTriggerRight(triggerId, otherId, onEnter, onLeave)
+    if g_currentMission.player ~= nil and g_currentMission.player.rootNode == otherId then
+        if onEnter then
+            self.triggerRightActive = true;
+        end
+        if onLeave then
+            self.triggerRightActive = false;
+        end
+        LTMaster.debug("self.triggerRightActive:%s", self.triggerRightActive);
+    end
+end
