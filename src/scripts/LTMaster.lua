77daccaf38cc5656479348a704b05488a36a3e04
Matteo Autieri <matteoautieri@live.it>
2017-04-19 19:20:27 +0200

    implemented ladder

--- src/scripts/LTMaster.lua
+++ src/scripts/LTMaster.lua
@@ -1,9 +1,9 @@
 --
--- Goweil LT Master
+--Goweil LT Master
 --
--- Team FSI Modding
+--Team FSI Modding
 --
--- 18/04/2017
+--18/04/2017
 LTMaster = {};
 LTMaster.debug = true
 
@@ -26,6 +26,7 @@ source(g_currentModDirectory .. "scripts/helpers/delayedCallBack.lua");
 source(g_currentModDirectory .. "scripts/events/hoodStatusEvent.lua");
 source(g_currentModDirectory .. "scripts/events/supportsStatusEvent.lua");
 source(g_currentModDirectory .. "scripts/events/foldingStatusEvent.lua");
+source(g_currentModDirectory .. "scripts/events/ladderStatusEvent.lua");
 
 function LTMaster.print(text, ...)
     if LTMaster.debug then
@@ -43,6 +44,8 @@ function LTMaster:preLoad(savegame)
     self.updateHoodStatus = LTMaster.updateHoodStatus;
     self.updateSupportsStatus = LTMaster.updateSupportsStatus;
     self.updateFoldingStatus = LTMaster.updateFoldingStatus;
+    self.updateLadderStatus = LTMaster.updateLadderStatus;
+    self.applyInitialAnimation = Utils.overwrittenFunction(self.applyInitialAnimation, LTMaster.applyInitialAnimation);
 end
 
 function LTMaster:load(savegame)
@@ -52,6 +55,8 @@ function LTMaster:load(savegame)
     self.LTMaster.triggerLeft = PlayerTrigger:new(trigger, Utils.getNoNil(getXMLFloat(self.xmlFile, "vehicle.LTMaster.triggers.triggerLeft#radius"), 2.5));
     trigger = Utils.indexToObject(self.components, getXMLString(self.xmlFile, "vehicle.LTMaster.triggers.triggerRight#index"));
     self.LTMaster.triggerRight = PlayerTrigger:new(trigger, Utils.getNoNil(getXMLFloat(self.xmlFile, "vehicle.LTMaster.triggers.triggerRight#radius"), 2.5));
+    trigger = Utils.indexToObject(self.components, getXMLString(self.xmlFile, "vehicle.LTMaster.triggers.triggerLadder#index"));
+    self.LTMaster.triggerLadder = PlayerTrigger:new(trigger, Utils.getNoNil(getXMLFloat(self.xmlFile, "vehicle.LTMaster.triggers.triggerLadder#radius"), 2.5));
     
     self.LTMaster.hoods = {};
     self.LTMaster.hoods.openingSound = SoundUtil.loadSample(self.xmlFile, {}, "vehicle.LTMaster.hoods.openingSound", nil, self.baseDirectory);
@@ -78,6 +83,12 @@ function LTMaster:load(savegame)
     self.LTMaster.folding.status = LTMaster.STATUS_FU_FOLDED;
     self.LTMaster.folding.delayedUpdateFoldingStatus = DelayedCallBack:new(LTMaster.updateFoldingStatus, self);
     self.LTMaster.folding.sound = SoundUtil.loadSample(self.xmlFile, {}, "vehicle.LTMaster.folding.sound", nil, self.baseDirectory);
+    
+    self.LTMaster.ladder = {};
+    self.LTMaster.ladder.animation = getXMLString(self.xmlFile, "vehicle.LTMaster.ladder#animationName");
+    self.LTMaster.ladder.status = LTMaster.STATUS_RL_RAISED;
+    self.LTMaster.ladder.delayedUpdateLadderStatus = DelayedCallBack:new(LTMaster.updateLadderStatus, self);
+    self.LTMaster.ladder.sound = SoundUtil.loadSample(self.xmlFile, {}, "vehicle.LTMaster.ladder.sound", nil, self.baseDirectory);
 end
 
 function LTMaster:postLoad(savegame)
@@ -87,6 +98,7 @@ function LTMaster:postLoad(savegame)
             self.LTMaster.hoods["right"].status = Utils.getNoNil(getXMLInt(savegame.xmlFile, savegame.key .. "#rightHoodStatus"), self.LTMaster.hoods["right"].status);
             self.LTMaster.supports.status = Utils.getNoNil(getXMLInt(savegame.xmlFile, savegame.key .. "#supportsStatus"), self.LTMaster.supports.status);
             self.LTMaster.folding.status = Utils.getNoNil(getXMLInt(savegame.xmlFile, savegame.key .. "#foldingStatus"), self.LTMaster.folding.status);
+            self.LTMaster.ladder.status = Utils.getNoNil(getXMLInt(savegame.xmlFile, savegame.key .. "#ladderStatus"), self.LTMaster.ladder.status);
         end
         LTMaster.finalizeLoad(self);
     end
@@ -96,8 +108,26 @@ function LTMaster:getSaveAttributesAndNodes(nodeIdent)
     local attributes = string.format("leftHoodStatus=\"%s\" rightHoodStatus=\"%s\" ", self.LTMaster.hoods["left"].status, self.LTMaster.hoods["right"].status);
     attributes = attributes .. string.format("supportsStatus=\"%s\" ", self.LTMaster.supports.status);
     attributes = attributes .. string.format("foldingStatus=\"%s\" ", self.LTMaster.folding.status);
+    attributes = attributes .. string.format("ladderStatus=\"%s\" ", self.LTMaster.ladder.status);
     local nodes = nil;
     return attributes, nodes;
+end
+
+function LTMaster:applyInitialAnimation()
+    --LTMaster.finalizeLoad(self);
+    if superFunc ~= nil then
+        superFunc(self);
+    end
+end
+
+function LTMaster:setRelativePosition(positionX, offsetY, positionZ, yRot)
+    --Called on setting position of vehicle (e. g. loading or reseting vehicle)
+    self.LTMaster.hoods["left"].status = LTMaster.STATUS_OC_CLOSED;
+    self.LTMaster.hoods["right"].status = LTMaster.STATUS_OC_CLOSED;
+    self.LTMaster.supports.status = LTMaster.STATUS_RL_RAISED;
+    self.LTMaster.folding.status = LTMaster.STATUS_FU_FOLDED;
+    self.LTMaster.ladder.status = LTMaster.STATUS_RL_RAISED;
+    LTMaster.finalizeLoad(self);
 end
 
 function LTMaster:finalizeLoad()
@@ -105,15 +135,18 @@ function LTMaster:finalizeLoad()
     self:updateHoodStatus(self.LTMaster.hoods["right"], nil, true);
     self:updateSupportsStatus(self.LTMaster.supports.status, true);
     self:updateFoldingStatus(self.LTMaster.folding.status, true);
+    self:updateLadderStatus(self.LTMaster.ladder.status, true);
 end
 
 function LTMaster:delete()
     self.LTMaster.triggerLeft:delete();
     self.LTMaster.triggerRight:delete();
+    self.LTMaster.triggerLadder:delete();
     SoundUtil.deleteSample(self.LTMaster.hoods.openingSound);
     SoundUtil.deleteSample(self.LTMaster.hoods.closingSound);
     SoundUtil.deleteSample(self.LTMaster.supports.sound);
     SoundUtil.deleteSample(self.LTMaster.folding.sound);
+    SoundUtil.deleteSample(self.LTMaster.ladder.sound);
 end
 
 function LTMaster:mouseEvent(posX, posY, isDown, isUp, button)
@@ -126,15 +159,16 @@ function LTMaster:update(dt)
     self.LTMaster.hoods.delayedUpdateHoodStatus:update(dt);
     self.LTMaster.supports.delayedUpdateSupportsStatus:update(dt);
     self.LTMaster.folding.delayedUpdateFoldingStatus:update(dt);
+    self.LTMaster.ladder.delayedUpdateLadderStatus:update(dt);
     if self.isClient then
-        -- Open/Close of the left door -->
+        --Open/Close of the left door -->
         if self.LTMaster.triggerLeft.active then
             if self.LTMaster.hoods["left"].status == LTMaster.STATUS_OC_OPEN then
                 g_currentMission:addHelpButtonText(g_i18n:getText("GLTM_CLOSE_HOOD"), InputBinding.IMPLEMENT_EXTRA2, nil, GS_PRIO_HIGH);
                 if InputBinding.hasEvent(InputBinding.IMPLEMENT_EXTRA2) then
                     self:updateHoodStatus(self.LTMaster.hoods["left"], LTMaster.STATUS_OC_CLOSING);
                 end
-                -- Raise/Lower of the supports -->
+                --Raise/Lower of the supports -->
                 if self.LTMaster.supports.status == LTMaster.STATUS_RL_RAISED then
                     g_currentMission:addHelpButtonText(g_i18n:getText("GLTM_LOWER_SUPPORTS"), InputBinding.IMPLEMENT_EXTRA4, nil, GS_PRIO_HIGH);
                     if InputBinding.hasEvent(InputBinding.IMPLEMENT_EXTRA4) then
@@ -146,7 +180,7 @@ function LTMaster:update(dt)
                     if InputBinding.hasEvent(InputBinding.IMPLEMENT_EXTRA4) then
                         self:updateSupportsStatus(LTMaster.STATUS_RL_RAISING);
                     end
-                    -- Fold/Unfold -->
+                    --Fold/Unfold -->
                     if self.LTMaster.folding.status == LTMaster.STATUS_FU_FOLDED then
                         g_currentMission:addHelpButtonText(g_i18n:getText("action_unfoldOBJECT"), InputBinding.IMPLEMENT_EXTRA, nil, GS_PRIO_HIGH);
                         if InputBinding.hasEvent(InputBinding.IMPLEMENT_EXTRA) then
@@ -159,9 +193,9 @@ function LTMaster:update(dt)
                             self:updateFoldingStatus(LTMaster.STATUS_FU_FOLDING);
                         end
                     end
-                -- Fold/Unfold <--
+                --Fold/Unfold <--
                 end
-            -- Raise/Lower of the supports <--
+            --Raise/Lower of the supports <--
             end
             if self.LTMaster.hoods["left"].status == LTMaster.STATUS_OC_CLOSED then
                 g_currentMission:addHelpButtonText(g_i18n:getText("GLTM_OPEN_HOOD"), InputBinding.IMPLEMENT_EXTRA2, nil, GS_PRIO_HIGH);
@@ -170,8 +204,8 @@ function LTMaster:update(dt)
                 end
             end
         end
-        -- Open/Close of the left door <--
-        -- Open/Close of the right door -->
+        --Open/Close of the left door <--
+        --Open/Close of the right door -->
         if self.LTMaster.triggerRight.active then
             if self.LTMaster.hoods["right"].status == LTMaster.STATUS_OC_OPEN then
                 g_currentMission:addHelpButtonText(g_i18n:getText("GLTM_CLOSE_HOOD"), InputBinding.IMPLEMENT_EXTRA2, nil, GS_PRIO_HIGH);
@@ -186,7 +220,23 @@ function LTMaster:update(dt)
                 end
             end
         end
-    -- Open/Close of the right door <--
+        --Open/Close of the right door <--
+        --Raise/Lower of the ladder -->
+        if self.LTMaster.triggerLadder.active then
+            if self.LTMaster.ladder.status == LTMaster.STATUS_RL_RAISED then
+                g_currentMission:addHelpButtonText(g_i18n:getText("GLTM_LOWER_LADDER"), InputBinding.IMPLEMENT_EXTRA2, nil, GS_PRIO_HIGH);
+                if InputBinding.hasEvent(InputBinding.IMPLEMENT_EXTRA2) then
+                    self:updateLadderStatus(LTMaster.STATUS_RL_LOWERING);
+                end
+            end
+            if self.LTMaster.ladder.status == LTMaster.STATUS_RL_LOWERED then
+                g_currentMission:addHelpButtonText(g_i18n:getText("GLTM_RAISE_LADDER"), InputBinding.IMPLEMENT_EXTRA2, nil, GS_PRIO_HIGH);
+                if InputBinding.hasEvent(InputBinding.IMPLEMENT_EXTRA2) then
+                    self:updateLadderStatus(LTMaster.STATUS_RL_RAISING);
+                end
+            end
+        end
+    --Raise/Lower of the ladder <--
     end
 end
 
@@ -196,6 +246,7 @@ function LTMaster:writeStream(streamId, connection)
         streamWriteUInt8(streamId, self.LTMaster.hoods["right"].status);
         streamWriteUInt8(streamId, self.LTMaster.supports.status);
         streamWriteUInt8(streamId, self.LTMaster.folding.status);
+        streamWriteUInt8(streamId, self.LTMaster.ladder.status);
     end
 end
 
@@ -205,6 +256,7 @@ function LTMaster:readStream(streamId, connection)
         self.LTMaster.hoods["right"].status = streamReadUInt8(streamId);
         self.LTMaster.supports.status = streamReadUInt8(streamId);
         self.LTMaster.folding.status = streamReadUInt8(streamId);
+        self.LTMaster.ladder.status = streamReadUInt8(streamId);
         LTMaster.finalizeLoad(self);
     end
 end
@@ -215,6 +267,7 @@ function LTMaster:writeUpdateStream(streamId, connection, dirtyMask)
         streamWriteUInt8(streamId, self.LTMaster.hoods["right"].status);
         streamWriteUInt8(streamId, self.LTMaster.supports.status);
         streamWriteUInt8(streamId, self.LTMaster.folding.status);
+        streamWriteUInt8(streamId, self.LTMaster.ladder.status);
     end
 end
 
@@ -224,6 +277,7 @@ function LTMaster:readUpdateStream(streamId, timestamp, connection)
         self.LTMaster.hoods["right"].status = streamReadUInt8(streamId);
         self.LTMaster.supports.status = streamReadUInt8(streamId);
         self.LTMaster.folding.status = streamReadUInt8(streamId);
+        self.LTMaster.ladder.status = streamReadUInt8(streamId);
     end
 end
 
@@ -313,5 +367,33 @@ function LTMaster:updateFoldingStatus(newStatus, noEventSend)
         SoundUtil.playSample(self.LTMaster.folding.sound, 0, 0, nil);
         self:playAnimation(self.LTMaster.folding.animation, -1);
         self.LTMaster.folding.delayedUpdateFoldingStatus:call(self:getAnimationDuration(self.LTMaster.folding.animation), LTMaster.STATUS_FU_FOLDED);
+    end
+end
+
+function LTMaster:updateLadderStatus(newStatus, noEventSend)
+    local status = newStatus or self.LTMaster.supports.status;
+    if not self.isServer and (noEventSend == nil or not noEventSend) then
+        g_client:getServerConnection():sendEvent(LadderStatusEvent:new(status, self));
+    end
+    if self.isServer then
+        self.LTMaster.ladder.status = status;
+    end
+    if status == LTMaster.STATUS_RL_LOWERED then
+        SoundUtil.stopSample(self.LTMaster.ladder.sound, true);
+        self:playAnimation(self.LTMaster.ladder.animation, math.huge);
+    end
+    if status == LTMaster.STATUS_RL_LOWERING then
+        SoundUtil.playSample(self.LTMaster.ladder.sound, 0, 0, nil);
+        self:playAnimation(self.LTMaster.ladder.animation, 1);
+        self.LTMaster.ladder.delayedUpdateLadderStatus:call(self:getAnimationDuration(self.LTMaster.ladder.animation), LTMaster.STATUS_RL_LOWERED);
+    end
+    if status == LTMaster.STATUS_RL_RAISED then
+        SoundUtil.stopSample(self.LTMaster.ladder.sound, true);
+        self:playAnimation(self.LTMaster.ladder.animation, -math.huge);
+    end
+    if status == LTMaster.STATUS_RL_RAISING then
+        SoundUtil.playSample(self.LTMaster.ladder.sound, 0, 0, nil);
+        self:playAnimation(self.LTMaster.ladder.animation, -1);
+        self.LTMaster.ladder.delayedUpdateLadderStatus:call(self:getAnimationDuration(self.LTMaster.ladder.animation), LTMaster.STATUS_RL_RAISED);
     end
 end
---
+++ src/scripts/events/ladderStatusEvent.lua
@@ -0,0 +1,36 @@
+--
+-- Goweil LT Master
+--
+-- Team FSI Modding
+--
+-- 19/04/2017
+LadderStatusEvent = {};
+LadderStatusEvent_mt = Class(LadderStatusEvent, Event);
+InitEventClass(LadderStatusEvent, "LadderStatusEvent");
+
+function LadderStatusEvent:emptyNew()
+    local self = Event:new(LadderStatusEvent_mt);
+    return self;
+end
+
+function LadderStatusEvent:new(status, vehicle)
+    local self = LadderStatusEvent:emptyNew();
+    self.status = status;
+    self.vehicle = vehicle;
+    return self;
+end
+
+function LadderStatusEvent:writeStream(streamId, connection)
+    streamWriteUInt8(streamId, self.status);
+    writeNetworkNodeObject(streamId, self.vehicle);
+end
+
+function LadderStatusEvent:readStream(streamId, connection)
+    self.status = streamReadUInt8(streamId);
+    self.vehicle = readNetworkNodeObject(streamId);
+    self:run(connection);
+end
+
+function LadderStatusEvent:run(connection)
+    self.vehicle.LTMaster.ladder.status = self.status;
+end
