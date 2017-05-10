fb28dc1027e451de41f1de5e8449ccec68bdad67
Matteo Autieri <matteoautieri@live.it>
2017-05-10 17:25:37 +0200

    @fcelsa, starts thinking on that

---
+++ src/scripts/baleInfos.lua
@@ -0,0 +1,27 @@
+--
+--Goweil LT Master
+--
+--TyKonKet - fcelsa (Team FSI Modding)
+--
+--10/05/2017
+BaleInfos = {};
+
+function BaleInfos:loadMap(name)
+end
+
+function BaleInfos:deleteMap()
+end
+
+function BaleInfos:keyEvent(unicode, sym, modifier, isDown)
+end
+
+function BaleInfos:mouseEvent(posX, posY, isDown, isUp, button)
+end
+
+function BaleInfos:update(dt)
+end
+
+function BaleInfos:draw()
+end
+
+addModEventListener(BaleInfos);
